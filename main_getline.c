a5a418134a43320a9c97bfcbb82d97ab4296ce44
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-30 09:12:40 -0800

    Updating shell.h

--- main_getline.c
+++ main_getline.c
@@ -1,20 +1,60 @@
 #include "shell.h"
 
 /**
+ * Global
+ */
+
+input *c;
+
+/**
  * main - uses getline and prints line
  * 
  * Return: 0
  */
-int m_getline(char **av)
+int m_getline()
 {
-    int i;
+    int i = 0;
     char *buff;
     size_t size;
+    const char *del = " \t\n";
+    input *b = c;
 
-    for (i = 1; av[i] != NULL; i++)
+    buff = malloc(sizeof(char *));
+    if (buff == NULL)
     {
-        printf("$%s\n", av[i]);
+    free(buff);
+    exit(1);
     }
-    return (0);
 
+    c = malloc(sizeof(input);
+    if (c == NULL)
+    {
+    free(c);
+    exit(1);
+    }
+
+    while (getline(&buff, &size, stdin) != -1)
+    {
+    if (i == 0)
+    {
+    c->command = strtok(buff, del);
+    i++;
+    }
+    if (i > 0)
+    {
+    if (c->arg->arg == NULL)
+    {
+    c->arg->arg = strtok(NULL, del);
+    }
+    else
+    {
+    c->arg->next = strtok(NULL, del);
+    }
+    c->arg = c->arg->next;
+    i++
+    }
+    }
+
+    free(buff);
+    return (0);
 }
--- shell.h
+++ shell.h
@@ -9,11 +9,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+/**
+ * Struct
+ */
+
+typedef struct input
+{
+	char *command;
+	s_args *arg;
+} input;
+
+typedef struct s_args
+{
+	char *arg;
+	struct s_args *next;
+} s_args;
+
+/**
+ * Global
+ */
+
+extern input *c;
+
 /**
  * Prototypes
  */
 
 int main(int ac, char *av[]);
-int m_getline(char **av);
+int m_getline();
 
 #endif
