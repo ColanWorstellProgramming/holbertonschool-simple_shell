f7b74e5be8adfcfc58d4caca8342f415f6c2a9b5
mnlazs <mnl.azs@gmail.com>
2022-12-01 13:50:03 -0800

    lol

---
+++ parssing.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+ * parse_cmd - 
+ * @input:
+ * Return: Array Of Char (Parsed)
+ */
+char **parse_cmd(char *input)
+{
+	char **tokens;
+	char *token;
+	int i, buffsize = BUFSIZE;
+
+	if (input == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(char *) * buffsize);
+	if (!tokens)
+	{
+		perror("hsh");
+		return (NULL);
+	}
+
+	token = _strtok(input, "\n ");
+	for (i = 0; token; i++)
+	{
+		tokens[i] = token;
+		token = _strtok(NULL, "\n ");
+	}
+	tokens[i] = NULL;
+
+	return (tokens);
+}
